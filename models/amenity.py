da47f7592d1de6e1526be599386dc8f979f9556a
hafdhisofien <1114@holbertonschool.com>
2020-02-19 14:00:20 +0000

    empty class

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is our amenity class that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    ublic class attributes:
+    name: string - empty string
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is our State class that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    name: string - empty string
+    """
+    name = ""
