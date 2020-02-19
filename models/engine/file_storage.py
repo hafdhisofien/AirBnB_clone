b2c085881c2c4b6ef33ff01cdf8696cfc8e3dfe7
hafdhisofien <1114@holbertonschool.com>
2020-02-19 14:11:17 +0000

    fixed import

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,12 @@ file storage class
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 classes = {"BaseModel": BaseModel, "User": User, "Place": Place,
            "State": State,
